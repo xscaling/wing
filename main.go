1891386aac82e22f86cc9978725701e5d1c674c3
Neo Zhuo <neowiz@icloud.com>
2022-11-23 11:02:50 +0000

    feat: add plugin discovery machinery

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ limitations under the License.
 
 package main
 
+//go:generate go run generate_plugin_discovery.go
+
 import (
 	"flag"
 	"os"
@@ -23,6 +25,9 @@ import (
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	// Register plugins
+	_ "github.com/xscaling/wing/core/engine/plugin"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
