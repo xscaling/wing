a6b3a89d4dca47464db0ca26fe42bc10c447c851
Neo Zhuo <neowiz@icloud.com>
2022-11-30 07:54:08 +0000

    chore: modify leader election ID

--- main.go
+++ main.go
@@ -87,7 +87,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "a9bf3adb.xscaling.dev",
+		LeaderElectionID:       "wing.xscaling.dev",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
