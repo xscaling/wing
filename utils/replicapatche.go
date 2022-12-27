9bb1992b2636221781ee9035ab3f44624873527d
Neo Zhuo <neowiz@icloud.com>
2022-12-27 17:42:07 +0800

    feat: add replica patch utils

---
+++ utils/replicapatche.go
@@ -0,0 +1,63 @@
+package utils
+
+import (
+	"encoding/json"
+	"time"
+
+	wingv1 "github.com/xscaling/wing/api/v1"
+	"github.com/xscaling/wing/utils/timerange"
+)
+
+func GetReplicaPatches(replicaAutoscaler wingv1.ReplicaAutoscaler) (wingv1.ReplicaPatches, error) {
+	if replicaAutoscaler.Annotations == nil {
+		return nil, nil
+	}
+	rawString, ok := replicaAutoscaler.Annotations[wingv1.ReplicaPatchesAnnotation]
+	if !ok {
+		return nil, nil
+	}
+	var replicaPatches wingv1.ReplicaPatches
+	err := json.Unmarshal([]byte(rawString), &replicaPatches)
+	if err != nil {
+		return nil, err
+	}
+	return replicaPatches, nil
+}
+
+func PurgeUnusedReplicaPatches(replicaAutoscaler *wingv1.ReplicaAutoscaler) error {
+	patches, err := GetReplicaPatches(*replicaAutoscaler)
+	if err != nil {
+		return err
+	}
+	if patches == nil {
+		return nil
+	}
+	var newPatches wingv1.ReplicaPatches
+	for _, patch := range patches {
+		timezone, err := time.LoadLocation(patch.Timezone)
+		if err == nil {
+			dateScheduler, err := timerange.NewDateScheduler(timezone, patch.Start, patch.End)
+			if err == nil {
+				expiredTime := dateScheduler.GetEndTime()
+				if patch.RetentionSeconds != nil {
+					expiredTime = expiredTime.Add(time.Duration(*patch.RetentionSeconds) * time.Second)
+				}
+				// Delete it
+				if time.Now().After(expiredTime) {
+					continue
+				}
+			}
+		}
+		newPatches = append(newPatches, patch)
+	}
+	if len(newPatches) == 0 {
+		delete(replicaAutoscaler.Annotations, wingv1.ReplicaPatchesAnnotation)
+		return nil
+	}
+	newPatchesString, err := json.Marshal(newPatches)
+	if err != nil {
+		return err
+	}
+	replicaAutoscaler.Annotations[wingv1.ReplicaPatchesAnnotation] = string(newPatchesString)
+	return nil
+}
