2ed6fb27143ed08269aac893ef71e53a50efd5a9
ChanceLi <1993lchsh@163.com>
2024-08-28 18:56:34 +0800

    feat: support message type filter for RabbitMQ

--- plugins/scaler_rabbitmq/api.go
+++ plugins/scaler_rabbitmq/api.go
@@ -5,6 +5,7 @@ import "fmt"
 type queueInfo struct {
 	Messages               int         `json:"messages"`
 	MessagesUnacknowledged int         `json:"messages_unacknowledged"`
+	MessageReady           int         `json:"messages_ready"`
 	MessageStat            messageStat `json:"message_stats"`
 	Name                   string      `json:"name"`
 }
@@ -17,63 +18,68 @@ type publishDetail struct {
 	Rate float64 `json:"rate"`
 }
 
-func getComposedQueue(operation Operation, q []queueInfo) (queueInfo, error) {
-	var queue = queueInfo{}
-	queue.Name = "composed-queue"
-	queue.MessagesUnacknowledged = 0
-	if len(q) > 0 {
-		switch operation {
-		case OperationSum:
-			sumMessages, sumRate := getSum(q)
-			queue.Messages = sumMessages
-			queue.MessageStat.PublishDetail.Rate = sumRate
-		case OperationAvg:
-			avgMessages, avgRate := getAverage(q)
-			queue.Messages = avgMessages
-			queue.MessageStat.PublishDetail.Rate = avgRate
-		case OperationMax:
-			maxMessages, maxRate := getMaximum(q)
-			queue.Messages = maxMessages
-			queue.MessageStat.PublishDetail.Rate = maxRate
-		default:
-			return queue,
-				fmt.Errorf("operation mode %s must be one of %s, %s, %s",
-					operation, OperationSum, OperationAvg, OperationMax)
-		}
-	} else {
-		queue.Messages = 0
-		queue.MessageStat.PublishDetail.Rate = 0
+func getComposedQueue(operation Operation, q []queueInfo) (queue queueInfo, err error) {
+	if len(q) == 0 {
+		return
 	}
-
+	operator := getSum
+	switch operation {
+	case OperationSum:
+		operator = getSum
+	case OperationAvg:
+		operator = getAverage
+	case OperationMax:
+		operator = getMaximum
+	default:
+		err = fmt.Errorf("operation mode %s must be one of %s, %s, %s",
+			operation, OperationSum, OperationAvg, OperationMax)
+		return
+	}
+	queue.Messages, queue.MessageReady, queue.MessagesUnacknowledged,
+		queue.MessageStat.PublishDetail.Rate = operator(q)
 	return queue, nil
 }
 
-func getSum(q []queueInfo) (int, float64) {
-	var sumMessages int
-	var sumRate float64
+func getSum(q []queueInfo) (
+	sumMessages int,
+	sumMessagesReady int,
+	sumMessagesUnacknowledged int,
+	sumRate float64,
+) {
 	for _, value := range q {
 		sumMessages += value.Messages
+		sumMessagesReady += value.MessageReady
+		sumMessagesUnacknowledged += value.MessagesUnacknowledged
 		sumRate += value.MessageStat.PublishDetail.Rate
 	}
-	return sumMessages, sumRate
+	return sumMessages, sumMessagesReady, sumMessagesUnacknowledged, sumRate
 }
 
-func getAverage(q []queueInfo) (int, float64) {
-	sumMessages, sumRate := getSum(q)
+func getAverage(q []queueInfo) (int, int, int, float64) {
+	sumMessages, sumMessagesReady, sumMessagesUnacknowledged, sumRate := getSum(q)
 	length := len(q)
-	return sumMessages / length, sumRate / float64(length)
+	return sumMessages / length, sumMessagesReady / length, sumMessagesUnacknowledged / length, sumRate / float64(length)
 }
 
-func getMaximum(q []queueInfo) (int, float64) {
-	var maxMessages int
-	var maxRate float64
+func getMaximum(q []queueInfo) (
+	maxMessages int,
+	maxMessagesReady int,
+	maxMessagesUnacknowledged int,
+	maxRate float64,
+) {
 	for _, value := range q {
 		if value.Messages > maxMessages {
 			maxMessages = value.Messages
+		}
+		if value.MessageReady > maxMessagesReady {
+			maxMessagesReady = value.MessageReady
+		}
+		if value.MessagesUnacknowledged > maxMessagesUnacknowledged {
+			maxMessagesUnacknowledged = value.MessagesUnacknowledged
 		}
 		if value.MessageStat.PublishDetail.Rate > maxRate {
 			maxRate = value.MessageStat.PublishDetail.Rate
 		}
 	}
-	return maxMessages, maxRate
+	return maxMessages, maxMessagesReady, maxMessagesUnacknowledged, maxRate
 }
