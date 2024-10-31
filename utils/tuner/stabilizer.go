ec46ecceb17f72899250c0c83fb50a5a8de0109c
Neo Zhuo <neowiz@icloud.com>
2024-10-31 15:46:48 +0800

    feat: implements `Stabilizer` replica tuner

---
+++ utils/tuner/stabilizer.go
@@ -0,0 +1,74 @@
+package tuner
+
+import (
+	"sync"
+	"time"
+)
+
+const (
+	DefaultStabilizationWindow    = time.Second * 30
+	DefaultReplicaMemoryMaxSize   = 100
+	DefaultReplicaMemoryRetention = time.Hour
+)
+
+var DefaultStabilizerPreference = StabilizerPreference{
+	ScaleUpStabilizationSeconds:   int(DefaultStabilizationWindow.Seconds()),
+	ScaleDownStabilizationSeconds: int(DefaultStabilizationWindow.Seconds()),
+}
+
+type Stabilizer struct {
+	historicalRecommendation sync.Map
+}
+
+func NewStabilizer() *Stabilizer {
+	return &Stabilizer{}
+}
+
+type StabilizerPreference struct {
+	ScaleUpStabilizationSeconds   int `json:"scaleUpStabilizationSeconds" yaml:"scaleUpStabilizationSeconds"`
+	ScaleDownStabilizationSeconds int `json:"scaleDownStabilizationSeconds" yaml:"scaleDownStabilizationSeconds"`
+}
+
+func (s *Stabilizer) GetRecommendation(keyForAutoscaler string,
+	currentReplicas int32, desiredReplicas int32, preference interface{}) int32 {
+	maxRecommendation := desiredReplicas
+	stabilizationWindow := DefaultStabilizationWindow
+	stabilizerPreference, ok := preference.(StabilizerPreference)
+	if !ok {
+		stabilizerPreference = DefaultStabilizerPreference
+	}
+
+	if desiredReplicas > currentReplicas {
+		stabilizationWindow = time.Duration(stabilizerPreference.ScaleUpStabilizationSeconds) * time.Second
+	} else {
+		stabilizationWindow = time.Duration(stabilizerPreference.ScaleDownStabilizationSeconds) * time.Second
+	}
+	if stabilizationWindow == 0 {
+		stabilizationWindow = DefaultStabilizationWindow
+	}
+	cutoff := time.Now().Add(-stabilizationWindow)
+
+	rm, ok := s.historicalRecommendation.Load(keyForAutoscaler)
+	if !ok {
+		rm = NewSimpleReplicaMemory(DefaultReplicaMemoryMaxSize, DefaultReplicaMemoryRetention)
+		s.historicalRecommendation.Store(keyForAutoscaler, rm)
+	}
+
+	events := rm.(ReplicaMemory).GetMemorySince(cutoff)
+
+	for _, rec := range events {
+		if rec.Replicas > maxRecommendation {
+			maxRecommendation = rec.Replicas
+		}
+	}
+
+	rm.(ReplicaMemory).Add(ReplicaSnapshot{
+		Timestamp: time.Now(),
+		Replicas:  desiredReplicas,
+	})
+	return maxRecommendation
+}
+
+// Stabilizer always accepts the recommendation.
+func (s *Stabilizer) AcceptRecommendation(keyForAutoscaler string, currentReplicas int32, desiredReplicas int32) {
+}
---
+++ utils/tuner/types.go
@@ -0,0 +1,24 @@
+package tuner
+
+type Tuner interface {
+	// GetRecommendation returns a recommended replica count based on the current state and preferences.
+	// Some tuners may not distinguish between scale up/down preferences, so the preference structure
+	// is left to each tuner's implementation to interpret appropriately.
+	GetRecommendation(keyForAutoscaler string,
+		currentReplicas int32, desiredReplicas int32, preference interface{}) int32
+	AcceptRecommendation(keyForAutoscaler string, currentReplicas int32, desiredReplicas int32)
+}
+
+func max(a, b int32) int32 {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int32) int32 {
+	if a <= b {
+		return a
+	}
+	return b
+}
