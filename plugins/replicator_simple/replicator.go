9c5f73b6e4b6d8148fa0af52b15914cbcfa6c475
Neo Zhuo <neowiz@icloud.com>
2024-11-12 15:36:33 +0800

    feat: integrate flux tuner with simple replicator

--- plugins/replicator_simple/replicator.go
+++ plugins/replicator_simple/replicator.go
@@ -1,98 +1,63 @@
 package simple
 
 import (
-	"errors"
-	"time"
+	"encoding/json"
 
 	wingv1 "github.com/xscaling/wing/api/v1"
 	"github.com/xscaling/wing/core/engine"
+	"github.com/xscaling/wing/utils/tuner"
 
 	"github.com/go-logr/logr"
-	"k8s.io/client-go/tools/record"
 )
 
 const (
-	DefaultDownscaleStabilizationWindow = time.Second * 30
+	PluginName = "simple"
 )
 
 type Config struct {
-	DownscaleStabilizationWindow time.Duration `yaml:"downscaleStabilizationWindow"`
+	DisableTuner bool              `json:"disableTuner" yaml:"disableTuner"`
+	Flux         tuner.FluxOptions `json:"flux" yaml:"flux"`
 }
 
 func NewDefaultConfig() *Config {
 	return &Config{
-		DownscaleStabilizationWindow: DefaultDownscaleStabilizationWindow,
+		Flux: tuner.NewDefaultFluxOptions(),
 	}
 }
 
 func (c Config) Validate() error {
-	if c.DownscaleStabilizationWindow < 0 {
-		return errors.New("downscale stabilization window must be positive")
-	}
 	return nil
 }
 
-type timestampedRecommendation struct {
-	timestamp time.Time
-	replicas  int32
+type Settings struct {
+	FluxPreference *tuner.FluxPreference
 }
 
 type replicator struct {
 	config Config
 	logger logr.Logger
 
-	eventRecorder            record.EventRecorder
-	historicalRecommendation map[string][]timestampedRecommendation
+	// Tuners
+	flux tuner.Tuner
 }
 
 func getUniqueKeyForAutoscaler(autoscaler *wingv1.ReplicaAutoscaler) string {
 	return autoscaler.Name + "/" + autoscaler.Namespace
 }
 
-// stabilizeRecommendation:
-// - replaces old recommendation with the newest recommendation,
-// - returns max of recommendations that are not older than downscaleStabilizationWindow.
-func (r *replicator) stabilizeRecommendation(key string, normalizedDesiredReplicas int32) int32 {
-	maxRecommendation := normalizedDesiredReplicas
-	foundOldSample := false
-	oldSampleIndex := 0
-	cutoff := time.Now().Add(-r.config.DownscaleStabilizationWindow)
-	for i, rec := range r.historicalRecommendation[key] {
-		if rec.timestamp.Before(cutoff) {
-			foundOldSample = true
-			oldSampleIndex = i
-		} else if rec.replicas > maxRecommendation {
-			maxRecommendation = rec.replicas
-		}
-	}
-	if foundOldSample {
-		r.historicalRecommendation[key][oldSampleIndex] = timestampedRecommendation{
-			timestamp: time.Now(),
-			replicas:  normalizedDesiredReplicas,
-		}
-	} else {
-		r.historicalRecommendation[key] = append(r.historicalRecommendation[key], timestampedRecommendation{
-			timestamp: time.Now(),
-			replicas:  normalizedDesiredReplicas,
-		})
-	}
-	return maxRecommendation
-}
-
 func (r *replicator) GetName() string {
-	return "simple"
+	return PluginName
 }
 
 func (r *replicator) GetDesiredReplicas(ctx engine.ReplicatorContext) (int32, error) {
 	logger := r.logger.WithValues("namespace", ctx.Autoscaler.Namespace, "replicaAutoscaler", ctx.Autoscaler.Name)
 
 	keyForAutoscaler := getUniqueKeyForAutoscaler(ctx.Autoscaler)
-	if r.historicalRecommendation[keyForAutoscaler] == nil {
-		r.historicalRecommendation[keyForAutoscaler] = []timestampedRecommendation{{
-			timestamp: time.Now(),
-			// Used expected replicas to avoid status pollution
-			replicas: ctx.Scale.Spec.Replicas,
-		}}
+	var settings Settings
+	err := json.Unmarshal(ctx.Autoscaler.Spec.ReplicatorSettings.Raw, &settings)
+	if err != nil {
+		logger.Error(err, "failed to unmarshal JSON into replicator setting")
+		return ctx.Autoscaler.Status.CurrentReplicas, err
 	}
 
 	var (
@@ -107,11 +72,16 @@ func (r *replicator) GetDesiredReplicas(ctx engine.ReplicatorContext) (int32, er
 		}
 	}
 
-	stabilizedReplicas := r.stabilizeRecommendation(keyForAutoscaler, desiredReplicas)
-	if stabilizedReplicas != desiredReplicas {
-		logger.V(2).Info("Stabilized desire replicas",
-			"normalizedDesiredReplicas", desiredReplicas, "stabilizedReplicas", stabilizedReplicas)
-		desiredReplicas = stabilizedReplicas
+	if r.flux != nil {
+		fluxReplicas := r.flux.GetRecommendation(keyForAutoscaler,
+			ctx.Autoscaler.Status.CurrentReplicas, desiredReplicas, settings.FluxPreference)
+		if fluxReplicas != desiredReplicas {
+			logger.V(2).Info("Fluxed desire replicas",
+				"normalizedDesiredReplicas", desiredReplicas, "fluxReplicas", fluxReplicas)
+			desiredReplicas = fluxReplicas
+		}
+		r.flux.AcceptRecommendation(keyForAutoscaler, ctx.Autoscaler.Status.CurrentReplicas, desiredReplicas)
 	}
+
 	return desiredReplicas, nil
 }
